5f094c18c0bda84e4dcc0854ba8d0687fc7ad564
Eric Spencer <spencer22eric@gmail.com>
2022-05-21 10:54:11 -0400

    add get single role route

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+// get all roles
 router.get('/roles', (req, res) => {
     const sql = 'SELECT * FROM role ORDER BY title';
 
@@ -14,6 +15,22 @@ router.get('/roles', (req, res) => {
         res.json({
             message: 'success',
             data: rows
+        });
+    });
+});
+
+// get one role by id
+router.get('/role/:id', (req, res) => {
+    const sql = `SELECT * FROM role WHERE id=?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+        res.json({
+            message: 'success',
+            data: row
         });
     });
 });
