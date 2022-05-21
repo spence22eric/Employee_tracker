58a53b131546a2e5bb399350fe47b2c7e3ab2556
Eric Spencer <spencer22eric@gmail.com>
2022-05-21 09:52:51 -0400

    add get route for single department

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,10 @@
 const mysql = require('mysql2');
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '12345',
     database: 'employees_db'
 });
-
-function insertDepartment() {
-    const sql = `INSERT INTO department `
-}
 
 module.exports = db;
--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -17,4 +17,20 @@ router.get('/departments', (req, res) => {
     });
 });
 
+router.get('/department/:id', (req, res) => {
+    const sql = `SELECT * FROM department WHERE id=?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+});
+
 module.exports = router;
