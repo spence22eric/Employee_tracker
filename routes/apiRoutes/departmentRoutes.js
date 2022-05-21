2e3070f14f7acfc0018c145b945a5c07bbdf3c06
Eric Spencer <spencer22eric@gmail.com>
2022-05-21 10:15:59 -0400

    add post route for department

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
 
 router.get('/departments', (req, res) => {
     const sql = `SELECT * FROM department ORDER BY name`
@@ -31,6 +32,33 @@ router.get('/department/:id', (req, res) => {
             data: row
         })
     })
+});
+
+router.post('/department', ({ body }, res) => {
+    const errors = inputCheck(
+        body,
+        'name'
+    );
+    if (errors) {
+        res.status(400).json({ error: errors});
+        return;
+    }
+
+    const sql = `INSERT INTO department (name) VALUES (?)`;
+    const params = [        
+        body.name
+    ];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
 });
 
 module.exports = router;
