ace6f7ce282b876dd37cc00c85fb5fb76ebe51a9
Eric Spencer <spencer22eric@gmail.com>
2022-05-30 15:51:31 -0400

    add role creation via inquirer

--- utils/query-utils/query-utils.js
+++ utils/query-utils/query-utils.js
@@ -1,6 +1,22 @@
 const questions = require('../../utils/questions');
 const cTable = require('console.table');
 
+// function verifyExists(db, answers) {
+//     const sql = `SELECT * FROM department WHERE id=?`;
+//     const params = [answers.roleDepartment];
+
+//     db.query(sql, params, (err, row) => {
+//         if (err) throw err;
+        
+//         if (!row || row == null || row == '') {
+//             console.log('That department does not exist!');
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     });    
+// }
+
 function queryDepartments(db) {
 
     const sql = `SELECT * FROM department`
@@ -38,7 +54,7 @@ function queryEmployees(db) {
     });
 }
 
-function addDepartment(db, answers) {
+function createDepartment(db, answers) {
     const sql = `INSERT INTO department (name) VALUES (?)`;
     const params = [
         answers.addDepartment
@@ -50,10 +66,27 @@ function addDepartment(db, answers) {
         console.log(`you have added ${answers.addDepartment} to the departments`);
         return questions();
     });
-    // console.log(answers.addDepartment);
 }
 
+function createRole(db, answers) {
+    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`
+    const params = [
+        answers.addRole,
+        answers.roleSalary,
+        answers.roleDepartment
+    ];   
+
+    db.query(sql, params, (err, result) => {
+        if (err) throw err;
+
+        console.log(`${answers.addRole} role created`)
+        return questions();
+    })
+}
+
+module.exports.createRole = createRole;
 module.exports.queryDepartments = queryDepartments;
 module.exports.queryRoles = queryRoles;
 module.exports.queryEmployees = queryEmployees;
-module.exports.addDepartment = addDepartment;
+module.exports.createDepartment = createDepartment;
+// module.exports.verifyExists = verifyExists;
--- utils/questions.js
+++ utils/questions.js
@@ -4,8 +4,8 @@ const queryUtils = require('./query-utils/query-utils');
 const db = require('../db/connection');
 
 
-module.exports = function () {    
-   
+module.exports = function () {
+
     inquirer
         .prompt([
             {
@@ -26,7 +26,7 @@ module.exports = function () {
             {
                 name: 'addDepartment',
                 type: 'input',
-                message:' Please enter the name of the department you would like to add.',
+                message: ' Please enter the name of the department you would like to add.',
                 when: (answer) => {
                     if (answer.options == 'Add a department') {
                         return true;
@@ -42,24 +42,89 @@ module.exports = function () {
                         return false;
                     }
                 }
+            },
+            {
+                name: 'addRole',
+                type: 'input',
+                message: 'Please enter the name of the role you would like to add.',
+                when: (answer) => {
+                    if (answer.options == 'Add a role') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: newRole => {
+                    if (newRole) {
+                        return true;
+                    } else {
+                        console.log('Please enter a valid role name.')
+                    }
+                }
+            },
+            {
+                name: 'roleSalary',
+                type: 'input',
+                message: 'Enter the role salary',
+                when: ({addRole}) => {
+                    if (addRole) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: answer => {
+                    if (isNaN(answer)) {
+                        console.log('   Error: Please enter a number');
+                        return false;
+                    } else if (!answer) {
+                        console.log('   Error: Please enter a number');
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                name: 'roleDepartment',
+                type: 'input',
+                message: 'Enter the associated department id',
+                when: ({roleSalary}) => {
+                    if (roleSalary) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: answer => {
+                    if (isNaN(answer)) {
+                        console.log('Please enter a number id corresponding to an existing department.')
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
             }
         ])
         .then(answers => {
             if (answers.options == 'View all departments') {
                 queryUtils.queryDepartments(db);
             }
-            
+
             if (answers.options == 'View all roles') {
                 queryUtils.queryRoles(db);
             }
-            
+
             if (answers.options == 'View all employees') {
                 queryUtils.queryEmployees(db);
             }
 
             if (answers.options == 'Add a department') {
-                queryUtils.addDepartment(db, answers);               
-            }            
-        });
+                queryUtils.createDepartment(db, answers);
+            }
 
+            if (answers.options == 'Add a role' && (answers.roleSalary) && (answers.roleDepartment)) {
+                queryUtils.createRole(db, answers);
+            }
+        });
 }
